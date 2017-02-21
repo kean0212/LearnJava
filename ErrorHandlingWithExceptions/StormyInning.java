8170f5e4a7f57edc222177890e9023d8307e984f
kean0212 <kean0212@hotmail.com>
2017-02-20 20:42:48 -0500

    Exception handling with inheritance.
    if the method of base class doesn’t throw Exception, then that of the
    derived class shouldn’t throw any Exception either. If the method of
    base class throws Exception, that of the derived class doesn’t have to
    throw Exception.

---
+++ ErrorHandlingWithExceptions/StormyInning.java
@@ -0,0 +1,84 @@
+class BaseballException extends Exception {
+}
+
+class Foul extends BaseballException {
+}
+
+class Strike extends BaseballException {
+}
+
+abstract class Inning {
+    public Inning() throws BaseballException {
+    }
+
+    public void event() throws BaseballException {
+    }
+
+    public abstract void atBat() throws Strike, Foul;
+
+    public void walk() {
+    }
+}
+
+class StormException extends Exception {
+}
+
+class RainedOut extends StormException {
+}
+
+class PopFoul extends Foul {
+}
+
+interface Storm {
+    public void event() throws RainedOut;
+
+    public void rainHard() throws RainedOut;
+}
+
+public class StormyInning extends Inning implements Storm {
+    public StormyInning() throws RainedOut, BaseballException {
+    }
+
+    public StormyInning(String string) throws Foul, BaseballException {
+
+    }
+
+    public void rainHard() throws RainedOut {
+
+    }
+
+    public void event() {
+
+    }
+
+    public void atBat() throws PopFoul {
+
+    }
+
+    public static void main(String[] args) {
+        try {
+            StormyInning stormyInning = new StormyInning();
+            stormyInning.atBat();
+        } catch (PopFoul e) {
+            System.out.println("Pop foul");
+        } catch (RainedOut e) {
+            System.out.println("Rained out");
+        } catch (BaseballException e) {
+            System.out.println("Generic baseball exception");
+        }
+
+        try {
+            Inning inning = new StormyInning();
+            inning.atBat();
+        } catch (Strike e) {
+            System.out.println("Strike");
+        } catch (Foul e) {
+            System.out.println("Foul");
+        } catch (RainedOut e) {
+            System.out.println("Rained out");
+        } catch (BaseballException e) {
+            System.out.println("Generic baseball exception");
+        }
+    }
+
+}
