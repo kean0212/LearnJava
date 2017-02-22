fa83bff3f7708ba9767eec6c16ef687910fc2444
kean0212 <kean0212@hotmail.com>
2017-02-21 23:45:08 -0500

    Exercise 26
    The main method can also have exception specification, which passed to
    the console instead.

---
+++ ErrorHandlingWithExceptions/Exercise26.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+public class Exercise26 {
+    public static void main(String[] args) throws Exception {
+        FileInputStream file = new FileInputStream("Exercise27.java");
+        file.close();
+    }
+}
