3ecf4c0b4b91e948bd7559e78c8790d2a08e191f
kean0212 <kean0212@hotmail.com>
2017-04-09 16:27:33 -0400

    Exercise 19 1. Arrays.equals(arrayOne, arrayTwo); // this method compares the elements of two arrays using <Object>.equals(Object object); 2. We need to override the function “public boolean equals(Object object)” method. Pay attention to the signature of the method, “public”, “boolean”, “equals”, “Object”.

---
+++ Arrays/CompareArrays.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+class CompareTestClass {
+    private int integer;
+
+    public CompareTestClass(int integer) {
+        this.integer = integer;
+    }
+
+    public String toString() {
+        return "" + integer;
+    }
+
+    public boolean equals(Object another) {
+        if (another == null || !(another instanceof CompareTestClass)) {
+            return false;
+        }
+        return this.integer == ((CompareTestClass) another).integer;
+    }
+}
+
+public class CompareArrays {
+    public static void main(String[] args) {
+        int size = 10;
+        CompareTestClass[] compareTestClassesOne = new CompareTestClass[size];
+        for (int i = 0; i < 10; ++i) {
+            compareTestClassesOne[i] = new CompareTestClass(5);
+        }
+        System.out.println(compareTestClassesOne + Arrays.toString(compareTestClassesOne));
+
+        CompareTestClass[] compareTestClassesTwo = new CompareTestClass[size];
+        for (int i = 0; i < 10; ++i) {
+            compareTestClassesTwo[i] = new CompareTestClass(5);
+        }
+        System.out.println(compareTestClassesTwo + " " + Arrays.toString(compareTestClassesOne));
+
+        System.out.println("array one == array two: " + Arrays.equals(compareTestClassesOne, compareTestClassesTwo));
+    }
+}
