b71d6e7fcc158e2875dbda5d46f1675336bb98dd
kean li <kean0212@hotmail.com>
2017-05-11 09:31:24 -0400

    Containers in Depth: Exercise 10 - ready to test.

--- ContainersInDepth/SortedSet.java
+++ ContainersInDepth/SortedSet.java
@@ -1,10 +1,35 @@
 import java.util.LinkedList;
 import java.util.Comparator;
+import java.util.Collection;
 import java.util.Set;
 
 public class SortedSet<E extends Comparable> extends LinkedList<E> implements Set<E> {
 
     public boolean add(E object) {
+        if (isEmpty()) {
+            // Empty set
+            super.add(0, object);
+            return true;
+        } else if (object.compareTo(getLast()) > 0) {
+            // Object is the largest
+            return super.add(object);
+        }
+        // Normal case
+        for (int i = 0, size = size(); i < size - 1; ++i) {
+            if (object.compareTo(get(i)) > 0 && object.compareTo(get(i + 1)) < 0) {
+                super.add(i + 1, object);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean addAll(Collection<? extends E> c) {
+        for (E element : c) {
+            if (!add(element)) {
+                return false;
+            }
+        }
         return true;
     }
 
@@ -15,7 +40,7 @@ public class SortedSet<E extends Comparable> extends LinkedList<E> implements Se
             }
 
             public boolean equals(E object) {
-                return true; // todo: need to fix
+                return this.equals(object);
             }
         };
     }
@@ -30,10 +55,25 @@ public class SortedSet<E extends Comparable> extends LinkedList<E> implements Se
 
     // [firstElement, lastElement)
     public SortedSet<E> subset(E fromElement, E toElement) {
+        SortedSet<E> result = new SortedSet<E>();
+        for (int i = 0, size = size(); i < size; ++i) {
+            E element = get(i);
+            if (element.compareTo(fromElement) >= 0 && element.compareTo(fromElement) < 0) {
+                result.add(element);
+                break;
+            }
+        }
         return new SortedSet<E>();
     }
 
     public SortedSet<E> tailSet(E fromElement) {
-        return new SortedSet<E>();
+        SortedSet<E> result = new SortedSet<E>();
+        for (int i = 0, size = size(); i < size; ++i) {
+            if (get(i).compareTo(fromElement) >= 0) {
+                result.addAll(i, this);
+                break;
+            }
+        }
+        return result;
     }
 }
