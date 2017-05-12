64a26224e967802595dbfcb6c96dad8ad15f99b5
kean li <kean0212@hotmail.com>
2017-05-12 09:32:06 -0400

    Containers in Depth: Exercise 13 - initial commit

--- ContainersInDepth/AssociativeArray.java
+++ ContainersInDepth/AssociativeArray.java
@@ -3,7 +3,45 @@ import java.util.TreeMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class AssociativeArray {
+public class AssociativeArray<K, V> {
+    private Object[][] pairs;
+    private int index;
+
+    public AssociativeArray(int length) {
+        pairs = new Object[length][2];
+    }
+
+    public void put(K key, V value) {
+        if (index >= pairs.length) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        pairs[index++] = new Object[]{key, value};
+    }
+
+    @SuppressWarnings("unchecked")
+    public V get(K key) {
+        for (int i = 0; i < index; ++i) {
+            if (key.equals(pairs[i][0])) {
+                return (V) pairs[i][1];
+            }
+        }
+        return null;
+    }
+
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < index; ++i) {
+            stringBuilder.append(pairs[i][0].toString());
+            stringBuilder.append("=");
+            stringBuilder.append(pairs[i][1].toString());
+            if (i != index - 1) {
+                stringBuilder.append(", ");
+            }
+        }
+        stringBuilder.append("\n");
+        return stringBuilder.toString();
+    }
+
     private static void fill(Map<String, String> map) {
         map.put("sky", "blue");
         map.put("grass", "green");
