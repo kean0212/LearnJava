23b22d560632600361bfb7b6f4380e94acfaf6e2
kean li <kean0212@hotmail.com>
2017-05-19 23:47:49 -0400

    Containers in Depth: Exercise 24 - part of the methods in the Set interface

--- ContainersInDepth/SimpleHashSetDemo.java
+++ ContainersInDepth/SimpleHashSetDemo.java
@@ -10,5 +10,15 @@ public class SimpleHashSetDemo {
 
         Util.println("simpleHashSet.addAll(countryNames): " + simpleHashSet.addAll(countryNames));
         Util.println("simpleHashSet: " + simpleHashSet);
+
+        simpleHashSet.clear();
+        Util.println("simpleHashSet.clear(): " + simpleHashSet);
+
+        simpleHashSet.addAll(countryNames);
+        String firstCountryName = Countries.names(1).get(0);
+        Util.println("simpleHashSet.contains(firstCountryName): " + simpleHashSet.contains(firstCountryName));
+
+        List<String> subCountryNames = Countries.names(5);
+        Util.println("simpleHashSet.containsAll(subCountryNames): " + simpleHashSet.containsAll(subCountryNames));
     }
 }
